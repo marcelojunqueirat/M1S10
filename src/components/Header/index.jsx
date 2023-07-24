d57750f9259e48632f60f28716542b5d9a627f04
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-07-24 20:50:04 -0300

    [M1S10] Ex 2

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Header() {
+  return (
+    <a className="header" href="#">
+      <img className="logo" src='logo.svg' />
+      <span>Farmácia Milagrosa</span>
+    </a>
+  )
+}
+
+export default Header
